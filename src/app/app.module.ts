eaec26359a16bd7859ebdc6be1364e64ca1a012b
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-08-19 01:34:50 +0530

    Initial Commit with router link option updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import {ServersService} from './servers/servers.service';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRouter: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'users', component: UsersComponent },
+  {path: 'servers', component: ServersComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    UsersComponent,
+    UserComponent,
+    ServerComponent,
+    EditServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRouter)
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
