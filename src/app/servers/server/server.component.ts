fcacbf4e813ae7f8e834cb04508ae9e4ddca2c9d
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-08-24 20:30:58 +0530

    Fixing Unknown Routes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ServersService} from '../servers.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,7 +10,9 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
@@ -23,4 +25,8 @@ export class ServerComponent implements OnInit {
       );
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route,
+    queryParamsHandling: 'preserve'});
+  }
 }
