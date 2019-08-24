fafb3acd8fe68e76d2fef3df3899ba4f76b23f5b
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-08-24 23:43:37 +0530

    Gaurd route and child route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {FormsModule} from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
+import {AuthService} from './auth.service';
+import {AuthGuard} from './auth-guard.service';
 
 
 
@@ -33,7 +35,7 @@ import {AppRoutingModule} from './app-routing.module';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
